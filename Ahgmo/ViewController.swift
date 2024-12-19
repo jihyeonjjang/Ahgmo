4d7d546c7ff6f2ca6dfa3afaf20281110cc12be8
jihyeonjjang <jihyeon9975@gmail.com>
2024-12-19 17:58:12 +0900

    Initial Commit

---
+++ Ahgmo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ahgmo
+//
+//  Created by 지현 on 12/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
