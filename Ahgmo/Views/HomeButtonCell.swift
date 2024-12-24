236d652e7cdca5696afa6abc9824fc24a12bd2c0
jihyeonjjang <jihyeon9975@gmail.com>
2024-12-24 16:02:05 +0900

    feat: Create HomeButtonCell file
    - Added file for reusable collection view cell

---
+++ Ahgmo/Views/HomeButtonCell.swift
@@ -0,0 +1,13 @@
+//
+//  HomeButtonCell.swift
+//  Ahgmo
+//
+//  Created by 지현 on 12/23/24.
+//
+
+import UIKit
+
+class HomeButtonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var categoryButton: UIButton!
+}
