390677cf1389945f359bd244230b68f726357c6c
jihyeonjjang <jihyeon9975@gmail.com>
2025-02-25 01:09:22 +0900

    feat: Update UI and functionalities in HomeViewController
    - Removed all tableView-related code and reimplemented it with collectionView
        - Updated diffableDataSource Sections (.main -> .category/.information) accordingly
    - Modified category section UI by updating the cell structure to wrap the label
    - Replaced HomeListCell with cellRegistration for the information section
        - Changed from dequeueReusableCell to dequeueConfiguredReusableCell
    - Refactored configureNavigation() function
        - Removed unnecessary UIBarButtonItems and split the function
    - Implemented multi-selection (checkbox) with editButtonItem in navigationItem
        - Added checkbox as accessories in cellRegistration
        - Implemented setEditing method
    - Added selectAllItems() function for select/deselect all during editing (functionality yet to be implemented)

--- Ahgmo/Home/HomeViewController.swift
+++ Ahgmo/Home/HomeViewController.swift
@@ -8,40 +8,34 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    
     @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var tableView: UITableView!
     
-    typealias CollectionItem = CategoryData
-    typealias TableItem = InfoData
-    
-    enum Section: Int {
-        case main
+    enum Section: Int, CaseIterable {
+        case category
+        case information
     }
     
-    class DataSource: UITableViewDiffableDataSource<Section, TableItem> {
-        override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-            return true
-        }
+    enum Item: Hashable {
+        case categoryItem(CategoryData)
+        case informationItem(InfoData)
         
-        override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-            if editingStyle == .delete {
-                if let identifierToDelete = itemIdentifier(for: indexPath) {
-                    var snapshot = self.snapshot()
-                    snapshot.deleteItems([identifierToDelete])
-                    apply(snapshot)
-                }
+        var title: String {
+            switch self {
+            case .categoryItem(let data):
+                return data.title
+            case .informationItem(let data):
+                return data.title
             }
         }
     }
     
     var categoryItems: [CategoryData] = CategoryData.list
     var infoItems: [InfoData] = InfoData.list
+    var selectedItems: Set<Item> = []
+    var isSelectAll: Bool = false
     
-    var numberOfInfoList = 3
-    
-    var collectionViewDataSource: UICollectionViewDiffableDataSource<Section, CollectionItem>!
-    var tableViewDataSource: DataSource!
+    var searchController: UISearchController!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -53,11 +47,6 @@ class HomeViewController: UIViewController {
         configureNavigationItem()
         embedSearchControl()
         configureCollectionView()
-        configureTableView()
-        
-        collectionView.collectionViewLayout = layout()
-        tableView.delegate = self
-        collectionView.delegate = self
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -65,54 +54,10 @@ class HomeViewController: UIViewController {
         navigationController?.setToolbarHidden(true, animated: true)
     }
     
-    private func configureCollectionView() {
-        collectionViewDataSource = UICollectionViewDiffableDataSource<Section, CollectionItem>(collectionView: collectionView) { collectionView, indexPath, item in
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCategoryCell", for: indexPath) as? HomeCategoryCell else { return nil }
-            cell.configure(item: item)
-            return cell
-        }
-        
-        var collectionViewSnapshot = NSDiffableDataSourceSnapshot<Section, CollectionItem>()
-        collectionViewSnapshot.appendSections([.main])
-        collectionViewSnapshot.appendItems([CategoryData(title: "모두보기")], toSection: .main)
-        collectionViewSnapshot.appendItems(categoryItems, toSection: .main)
-        collectionViewDataSource.apply(collectionViewSnapshot)
-    }
-    
-    private func configureTableView() {
-        tableViewDataSource = DataSource(tableView: tableView) { tableView, indexPath, item in
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "HomeListCell", for: indexPath) as? HomeListCell else { return UITableViewCell() }
-            cell.configure(item: item)
-            return cell
-        }
-        
-        var tableViewSnapshot = NSDiffableDataSourceSnapshot<Section, TableItem>()
-        tableViewSnapshot.appendSections([.main])
-        tableViewSnapshot.appendItems(infoItems, toSection: .main)
-        tableViewDataSource.apply(tableViewSnapshot)
-    }
-    
     private func configureNavigationItem() {
         self.navigationItem.title = "아그모"
-        
-        let numberInfoLabel = labelBarButtonItem(text: "\(numberOfInfoList)개의 항목")
-        let numberSelectLabel = labelBarButtonItem(text: "0개 선택")
-        
-        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-        
-        let selectItem = UIBarButtonItem(
-            image: UIImage(systemName: "checklist"),
-            style: .plain,
-            target: self,
-            action: #selector(toggleEditing)
-        )
-        
-        let doneItem = UIBarButtonItem(
-            title: "완료",
-            style: .plain,
-            target: self,
-            action: #selector(toggleEditing)
-        )
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.largeTitleDisplayMode = .always
         
         let settingItem = UIBarButtonItem(
             image: UIImage(systemName: "gearshape"),
@@ -121,6 +66,25 @@ class HomeViewController: UIViewController {
             action: #selector(navigateToPage(_:))
         )
         settingItem.tag = 1
+        
+        navigationItem.leftBarButtonItem = editButtonItem
+        let leftBarButtonCustom = isEditing ? "완료" : "편집"
+        navigationItem.leftBarButtonItem?.title = leftBarButtonCustom
+        navigationItem.rightBarButtonItem = settingItem
+        
+//        let items: [UIBarButtonItem] = isEditing ? editToolBarItem() : defaultToolBarItem()
+        let items: [UIBarButtonItem] = defaultToolBarItem()
+        self.toolbarItems = items
+    }
+    
+    private func defaultToolBarItem() -> [UIBarButtonItem] {
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        
+        let numberInfoLabel = UIBarButtonItem(customView: {
+            let label = UILabel()
+            label.text = "\(infoItems.count)개의 항목"
+            return label
+        }())
         
         let categoryItem = UIBarButtonItem(
             image: UIImage(systemName: "folder"),
@@ -137,6 +101,26 @@ class HomeViewController: UIViewController {
         )
         plusItem.tag = 3
         
+        let defaultToolBarItems = [categoryItem, flexibleSpace, numberInfoLabel, flexibleSpace, plusItem]
+        return defaultToolBarItems
+    }
+    
+    private func editToolBarItem() -> [UIBarButtonItem] {
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let numberSelectLabel = UIBarButtonItem(customView: {
+            let label = UILabel()
+            label.text = "\(selectedItems.count)개 선택"
+            return label
+        }())
+        
+        let selectAllButton = UIBarButtonItem(
+            title: isSelectAll ? "전체 선택 해제" : "전체 선택",
+            style: .plain,
+            target: self,
+            action: #selector(selectAllItems)
+        )
+        selectAllButton.tag = 11
+        
         let deleteItem = UIBarButtonItem(
             title: "삭제",
             style: .plain,
@@ -144,19 +128,8 @@ class HomeViewController: UIViewController {
             action: #selector(deleteSelectedItems)
         )
         
-        navigationItem.leftBarButtonItem = tableView.isEditing ? doneItem : selectItem
-        navigationItem.rightBarButtonItem = settingItem
-        
-        let defaultToolBarItems = [categoryItem, flexibleSpace, numberInfoLabel, flexibleSpace, plusItem]
-        let editToolBarItems = [flexibleSpace, numberSelectLabel, flexibleSpace, deleteItem]
-        let items: [UIBarButtonItem] = tableView.isEditing ? editToolBarItems : defaultToolBarItems
-        self.toolbarItems = items
-    }
-    
-    private func labelBarButtonItem(text: String) -> UIBarButtonItem {
-        let label = UILabel()
-        label.text = "\(text)"
-        return UIBarButtonItem(customView: label)
+        let editToolBarItems = [selectAllButton, flexibleSpace, numberSelectLabel, flexibleSpace, deleteItem]
+        return editToolBarItems
     }
     
     @objc private func navigateToPage(_ sender: UIBarButtonItem) {
@@ -178,17 +151,41 @@ class HomeViewController: UIViewController {
         }
     }
     
-    @objc private func toggleEditing() {
-        tableView.setEditing(!tableView.isEditing, animated: true)
-        configureNavigationItem()
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        
+        let leftBarButtonCustom = isEditing ? "완료" : "편집"
+        navigationItem.leftBarButtonItem?.title = leftBarButtonCustom
+        let items: [UIBarButtonItem] = isEditing ? editToolBarItem() : defaultToolBarItem()
+        self.toolbarItems = items
+        if !isEditing {
+            if !selectedItems.isEmpty {
+                selectedItems.removeAll()
+                collectionView.reloadData()
+            }
+        }
+        collectionView.isEditing = editing
+    }
+
+    @objc private func selectAllItems() {
+        isSelectAll.toggle()
+        if isSelectAll {
+//            selectedItems.insert()
+//            updateSnapshot(section: .information)
+        } else {
+            selectedItems.removeAll()
+        }
+        if let selectAllButton = self.toolbarItems?.first(where: { $0.tag == 11 }) {
+            selectAllButton.title = isSelectAll ? "전체 선택 해제" : "전체 선택"
+        }
     }
     
     @objc private func deleteSelectedItems() {
-        // 선택된 항목 삭제
+        
     }
     
     private func embedSearchControl() {
-        let searchController = UISearchController(searchResultsController: nil)
+        searchController = UISearchController(searchResultsController: nil)
         searchController.hidesNavigationBarDuringPresentation = false
         searchController.searchBar.placeholder = "Search"
         searchController.searchBar.delegate = self
@@ -196,47 +193,148 @@ class HomeViewController: UIViewController {
         self.navigationItem.hidesSearchBarWhenScrolling = false
     }
     
+    private func accessoryCustomView(imageName: String) -> UIImageView {
+        let accessoryimageView = UIImageView(image: UIImage(systemName: "\(imageName)")?.withRenderingMode(.alwaysTemplate))
+        accessoryimageView.tintColor = .systemGray3
+        
+        return accessoryimageView
+    }
+    
+    private func configureCollectionView() {
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Item> { cell, indexPath, item in
+            var content = UIListContentConfiguration.cell()
+            content.text = item.title
+            cell.contentConfiguration = content
+            
+            let circleAccessory = UICellAccessory.CustomViewConfiguration(
+                customView: self.accessoryCustomView(imageName: "circle"),
+                placement: .leading(displayed: .whenEditing)
+            )
+            
+            let checkAccessory = UICellAccessory.CustomViewConfiguration(
+                customView: UIImageView(image: UIImage(systemName: "checkmark.circle.fill")?.withRenderingMode(.alwaysOriginal)),
+                placement: .leading(displayed: .whenEditing)
+            )
+            
+            var accessories: [UICellAccessory] = [.customView(configuration: circleAccessory)]
+            
+            if self.selectedItems.contains(item) {
+                accessories = [.customView(configuration: checkAccessory)]
+            }
+            cell.accessories = accessories
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView) { collectionView, indexPath, item in
+            switch item {
+            case .categoryItem(let item):
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCategoryCell", for: indexPath) as? HomeCategoryCell else {
+                    fatalError("error")
+                }
+                cell.backgroundColor = item.isSelected ? .systemBlue : .secondarySystemBackground
+                cell.layer.masksToBounds = true
+                cell.layer.cornerRadius = 17
+                cell.configure(item: item)
+                return cell
+            case .informationItem:
+                return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
+            }
+        }
+        
+        applySnapshot()
+        
+        collectionView.collectionViewLayout = layout()
+        collectionView?.contentInsetAdjustmentBehavior = .never
+        collectionView.delegate = self
+        collectionView.allowsSelectionDuringEditing = true
+        collectionView.keyboardDismissMode = .onDrag
+    }
+    
+    private func applySnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections(Section.allCases)
+        snapshot.appendItems([CategoryData(title: "모두보기", isSelected: true)].map { Item.categoryItem($0) }, toSection: .category)
+        snapshot.appendItems(categoryItems.map { Item.categoryItem($0) }, toSection: .category)
+        snapshot.appendItems(infoItems.map{ Item.informationItem($0) }, toSection: .information)
+        dataSource.apply(snapshot)
+    }
+    
+    private func updateSnapshot(item: Item) {
+        var snapshot = dataSource.snapshot()
+        snapshot.reloadItems([item])
+        dataSource.apply(snapshot)
+    }
+    
     private func layout() -> UICollectionViewCompositionalLayout {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .estimated(100), heightDimension: .absolute(40))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(100), heightDimension: .absolute(40))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        //        group.interItemSpacing = NSCollectionLayoutSpacing.fixed(7)
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 0)
-        section.interGroupSpacing = 10
-        section.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
-        
-        return UICollectionViewCompositionalLayout(section: section)
+        return UICollectionViewCompositionalLayout { (section, env) -> NSCollectionLayoutSection? in
+            if section == 0  {
+                let itemSize = NSCollectionLayoutSize(widthDimension: .estimated(50), heightDimension: .absolute(35))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                
+                let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(50), heightDimension: .absolute(35))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 0)
+                section.interGroupSpacing = 10
+                section.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
+                
+                return section
+            } else {
+                let config = UICollectionLayoutListConfiguration(appearance: .plain)
+                let section = NSCollectionLayoutSection.list(using: config, layoutEnvironment: env)
+                section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0)
+                
+                return section
+            }
+        }
     }
 }
 
-extension HomeViewController: UISearchBarDelegate, UICollectionViewDelegate, UITableViewDelegate {
-    // 동작안함
+extension HomeViewController: UICollectionViewDelegate, UISearchBarDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let item = categoryItems[indexPath.item]
-        print(">>> selected: \(item.title)")
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let item = infoItems[indexPath.item]
-        print(">>> selected: \(item.title)")
-        
-        if tableView.isEditing {
-            return
+        switch Section(rawValue: indexPath.section) {
+        case .category:
+            if indexPath.item == 0 {
+                print(">>> selected: 모두보기")
+                
+            } else {
+                var item = categoryItems[indexPath.item - 1]
+                print(">>> selected: \(item.title)")
+                
+                // toggle이 안되는데?
+                item.isSelected.toggle()
+                print(">>> selected: \(item.isSelected)")
+            }
+        default:
+            if isEditing {
+                guard let item = dataSource.itemIdentifier(for: indexPath) else { return }
+                
+                if selectedItems.contains(item) {
+                    selectedItems.remove(item)
+                } else {
+                    selectedItems.insert(item)
+                }
+                updateSnapshot(item: item)
+            } else {
+                let item = infoItems[indexPath.item]
+                print(">>> selected: \(item.title)")
+                
+                let storyboard = UIStoryboard(name: "DetailInfo", bundle: nil)
+                let vc = storyboard.instantiateViewController(withIdentifier: "DetailInfoViewController") as! DetailInfoViewController
+                vc.information = item
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
         }
-        
-        let storyboard = UIStoryboard(name: "DetailInfo", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "DetailInfoViewController") as! DetailInfoViewController
-        vc.information = item
-        self.navigationController?.pushViewController(vc, animated: true)
-        tableView.deselectRow(at: indexPath, animated: true)
+        collectionView.deselectItem(at: indexPath, animated: true)
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
         guard let keyword = searchBar.text, !keyword.isEmpty else { return }
-        
+        print("\(keyword)")
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        collectionView.reloadData()
     }
 }
